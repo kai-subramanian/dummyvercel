855bf5a3b9d3cfdcda631dd1e8e40446bc7b8d1c
kai-subramanian <skailash27082k@gmail.com>
2025-01-11 15:56:33 +0530

    debug1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import * as tailwindAspect from "@tailwindcss/aspect-ratio"
 export default {
   content: [
     "./index.html",
@@ -7,5 +8,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio')],
+  plugins: [tailwindAspect],
 }
